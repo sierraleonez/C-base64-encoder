debed6e3274c93101d79d6329c95f9bca1a2bcd0
Almer Andhika <almer@MIFX-IT-ALMER.local>
2026-06-25 21:02:31 +0700

    feat: add padding and handle empty character

--- main.c
+++ main.c
@@ -1,40 +1,112 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 const char base64Table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-void encodeBase64(const char *input)
+void printChunk(char input)
 {
-    int i = 0;
-    unsigned char currentChar = input[i];
-    unsigned char nextChar = input[i + 1];
-    unsigned char thirdChar = input[i + 2];
-
-    // 1. create the first chunk, 6 leftmost bits
-    unsigned char firstChunk = currentChar >> 2;
-    // 2. 2nd chunk, last 2 bit of first character + 4 first bit of 2nd character
-    unsigned char secondChunk = (currentChar & 0b00000011) << 4 | (nextChar & 0b11110000) >> 4;
-    // 3. 3rd chunk, last 4 bit of 2nd character + first 2 bit of third character
-    unsigned char thirdChunk = ((nextChar & 0b00001111) << 2) | ((thirdChar & 0b11000000) >> 6);
-    // 4. 4th chunk, last 6 bit of 3rd character
-    unsigned char fourthChunk = thirdChar & (0b00111111);
-
-    char firstChunkTranslated = base64Table[firstChunk];
-    char secondChunkTranslated = base64Table[secondChunk];
-    char thirdChunkTranslated = base64Table[thirdChunk];
-    char fourthChunkTranslated = base64Table[fourthChunk];
-
-    printf("result: %c %c %c %c \n", firstChunkTranslated, secondChunkTranslated, thirdChunkTranslated, fourthChunkTranslated);
-
+    printf("%c", input);
 }
 
 
+bool checkEndOfChar(char input)
+{
+    if (input == '\0')
+        return true;
+    return false;
+}
+
+
+/**
+ * first char 6 bit
+ */
+void createFirstChunk(char firstChar)
+{
+    unsigned char firstChunk = firstChar >> 2;
+    char chunkTranslated = base64Table[firstChunk];
+
+    printChunk(chunkTranslated);
+}
+
+/**
+ * first char 2 bit
+ * second char 4 bit
+ */
+void createSecondChunk(char firstChar, char secondChar)
+{
+    unsigned char firstCharChunk = (firstChar & 0b00000011) << 4;
+    unsigned char secondCharChunk = checkEndOfChar(secondChar) ? 0b00 : (secondChar & 0b11110000) >> 4;
+
+    unsigned char chunk = firstCharChunk | secondCharChunk;
+    char chunkTranslated = base64Table[chunk];
+
+    printChunk(chunkTranslated);
+}
+
+/**
+ * second char 4 bit
+ * third char 2 bit
+ */
+void createThirdChunk(char secondChar, char thirdChar)
+{
+    if (checkEndOfChar(secondChar))
+    {
+        printChunk('=');
+        return;
+    }
+    else
+    {
+        
+        unsigned char secondCharChunk = (secondChar & 0b00001111) << 2;
+        unsigned char thirdCharChunk = checkEndOfChar(thirdChar) ? 0b00 : ((thirdChar & 0b11000000) >> 6);
+
+        unsigned char chunk = secondCharChunk | thirdCharChunk;
+        char chunkTranslated = base64Table[chunk];
+
+        printChunk(chunkTranslated);
+    }
+}
+
+void createFourthChunk(char secondChar, char thirdChar)
+{
+    if (checkEndOfChar(secondChar) || checkEndOfChar(thirdChar))
+    {
+        printChunk('=');
+    }
+    else
+    {
+        char thirdCharChunk = thirdChar & 0b00111111;
+        char chunkTranslated = base64Table[thirdCharChunk];
+        printChunk(chunkTranslated);
+    }
+}
+
+void encodeBase64Chunk(const char *input)
+{
+    int i = 0;
+    unsigned char firstChar = input[i];
+    unsigned char secondChar = input[i + 1];
+    unsigned char thirdChar = input[i + 2];
+    
+    createFirstChunk(firstChar);
+    createSecondChunk(firstChar, secondChar);
+    createThirdChunk(secondChar, thirdChar);
+    createFourthChunk(secondChar, thirdChar);
+}
+
+void encodeBase64(const char *input)
+{
+    encodeBase64Chunk(input);   
+}
+
 int main()
 {
-    const char *args = "abcdef";
-    printf("Input: %s\n", args);
+    const char *args = "abc";
+    const char *args2 = "xyz";
 
     printf("Encoded: ");
     encodeBase64(args);
+    encodeBase64(args2);
     return 0;
 }
